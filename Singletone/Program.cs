727edd0d4be74367d77b65ff51e18aa6da8ce47e
Alessandro <alessandro.seixas@gmail.com>
2021-04-27 09:10:05 -0300

    Publicação do projeto Singleton

---
+++ Singletone/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Singletone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("O Bahia está em campo e vai começar o jogo para a final do mundial de Clubes 2022 Bahia x Real Madrid!!! BBMP!!!!!");
+            Ball ball1 = Ball.GetInstanceWithSingleton();
+            ball1.Message("Galvão Bueno:Gilberto toca na bola e começa a partida, partiu Gilberto e da excelente toque para Rossi");
+            
+            
+            Ball ball2 = Ball.GetInstanceWithSingleton();
+            ball2.Message("Galvão Bueno:Rossi Recebe a Bola tenta driblar o zagueiro do Vitória que coloca a bola para lateral");
+
+            Ball ball4 = Ball.GetInstanceWithoutSingleton();
+            ball4.Message("Galvão Bueno:O Bandeirinha jogou mais uma bola em campo");
+
+            Ball ball3 = Ball.GetInstanceWithSingleton();
+            ball3.Message("Galvão Bueno:Nino Paraíba pega rapidamente a bola e toca pra Gilberto que faz o gol!!! Gollllllll do Baêaaaaa, BBMP!!!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singletone/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Singletone
+{
+        public class Ball
+        {
+            private static Ball instance = null;
+
+           // public static Ball Instance { get =>  instance = instance == null?new Ball(): instance; }
+
+            public static Ball GetInstanceWithSingleton()
+            {
+                if(instance == null)
+                {
+                    instance  = new Ball();
+                    Console.WriteLine("A Bola esta em jogo");
+              
+                }
+                return instance;
+            }
+
+            public static Ball GetInstanceWithoutSingleton()
+            {
+                instance = new Ball();
+                Console.WriteLine("A Bola esta em jogo");
+
+                return instance;
+            }
+
+
+        public void Message(string message)
+            { 
+                Console.WriteLine(message);
+            }
+        }
+}
+ 
